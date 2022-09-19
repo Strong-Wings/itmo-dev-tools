09a317223b9a14f69c0622e1dae10da7b6c674a8
Stepan Kryzhankov <transport777777@gmail.com>
2022-09-19 22:08:39 +0300

    feat: add summary method

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,8 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!"); 
     }
+
+    private static int sum(int a, int b) {
+        return a + b;
+    }
 }
